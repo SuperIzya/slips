e8b2739c9de3d1beb1d4bdba642a737a5a92cb5f
Ilya Kazovsky <gkazovsky@gmail.com>
2022-11-20 13:40:46 +0100

    Update index.js

--- .github/action-badges/index.js
+++ .github/action-badges/index.js
@@ -4,7 +4,7 @@ const fs = require('fs/promises');
 
 core.debug('Starting updating README.md');
 const badgesJSON = core.getInput('badges', {required: true});
-core.debug(`Passed badges configuration is ${badgesJSON}`);
+core.debug(`Passed badges configuration is ${badgesJSON} of type ${typeof badgesJSON}`);
 const template = (() => {
     const res = core.getInput('template');
     if(!res) return 'README.md.tpl';
@@ -16,7 +16,7 @@ const buildBadge = ({ref_name, repository}) => ({name, yaml}) =>
 
 async function run() {
     try {
-        const badges = badgesJSON.map(buildBadge(github)).reduce((a, b) => `${a} ${b}`, '');
+        const badges = JSON.parse(`${badgesJSON}`).map(buildBadge(github)).reduce((a, b) => `${a} ${b}`, '');
         core.debug(`The following badges will be added: ${badges}`);
         const data = await fs.readFile(`${github.workspace}/${template}`, {encoding: 'utf8'});
         core.debug('Writing README.md');
