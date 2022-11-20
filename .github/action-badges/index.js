61fe22258bb5f537a16bb0e41f55463f37b8b32d
Ilya Kazovsky <gkazovsky@gmail.com>
2022-11-20 13:38:55 +0100

    Update index.js

--- .github/action-badges/index.js
+++ .github/action-badges/index.js
@@ -16,7 +16,7 @@ const buildBadge = ({ref_name, repository}) => ({name, yaml}) =>
 
 async function run() {
     try {
-        const badges = JSON.parse(badgesJSON).map(buildBadge(github)).reduce((a, b) => `${a} ${b}`, '');
+        const badges = badgesJSON.map(buildBadge(github)).reduce((a, b) => `${a} ${b}`, '');
         core.debug(`The following badges will be added: ${badges}`);
         const data = await fs.readFile(`${github.workspace}/${template}`, {encoding: 'utf8'});
         core.debug('Writing README.md');
